ce20442b2982c6c9ad0a5415abeebb3de4174b99
dakamakat <gazon-ozon@yandex.ru>
2024-08-20 13:28:38 +0200

    feat(parser_builder): parse name

--- src/builders/parser_builder.rs
+++ src/builders/parser_builder.rs
@@ -1,10 +1,29 @@
 fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
-    move |input| {
-        match input.strip_prefix(expected) {
-            Some(remainder) => Ok((remainder, ())),
-            None => Err(input),
+    move |input| match input.strip_prefix(expected) {
+        Some(remainder) => Ok((remainder, ())),
+        None => Err(input),
+    }
+}
+
+fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
         }
     }
+
+    let next_index = matched.len();
+    Ok((&input[next_index..], matched))
 }
 
 #[cfg(test)]
@@ -20,5 +39,21 @@ mod tests {
             parse_joe("Hello Joe! Hello Robert!")
         );
         assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+    }
+
+    #[test]
+    fn identifier_parser() {
+        assert_eq!(
+            Ok(("", "i-am-an-identifier".to_string())),
+            identifier("i-am-an-identifier")
+        );
+        assert_eq!(
+            Ok((" entirely an identifier", "not".to_string())),
+            identifier("not entirely an identifier")
+        );
+        assert_eq!(
+            Err("!not at all an identifier"),
+            identifier("!not at all an identifier")
+        );
     }
 }
