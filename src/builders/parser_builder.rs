5ed39a9cec0c0d01d8c17f133f1362410ed662a1
dakamakat <gazon-ozon@yandex.ru>
2024-08-20 12:09:57 +0200

    feat(builders): add default structure / match first

---
+++ src/builders/parser_builder.rs
@@ -0,0 +1,22 @@
+fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
+    move |input| match input.get(0..expected.len()) {
+        Some(next) if next == expected => Ok((&input[expected.len()..], ())),
+        _ => Err(input),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn literal_parser() {
+        let parse_joe = match_literal("Hello Joe!");
+        assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
+        assert_eq!(
+            Ok((" Hello Robert!", ())),
+            parse_joe("Hello Joe! Hello Robert!")
+        );
+        assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+    }
+}
---
+++ src/models/element.rs
@@ -0,0 +1,5 @@
+struct Element {
+    name: String,
+    attributes: Vec<(String, String)>,
+    children: Vec<Element>,
+}
