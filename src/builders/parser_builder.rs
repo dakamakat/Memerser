d62613c183d77f212423fe9785cf42fdd7288740
dakamakat <gazon-ozon@yandex.ru>
2024-08-20 12:21:39 +0200

    chore(builders): use builtin prefix match

--- src/builders/parser_builder.rs
+++ src/builders/parser_builder.rs
@@ -1,7 +1,9 @@
 fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
-    move |input| match input.get(0..expected.len()) {
-        Some(next) if next == expected => Ok((&input[expected.len()..], ())),
-        _ => Err(input),
+    move |input| {
+        match input.strip_prefix(expected) {
+            Some(remainder) => Ok((remainder, ())),
+            None => Err(input),
+        }
     }
 }
 
